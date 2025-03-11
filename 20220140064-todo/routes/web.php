de6e4c73294a8e869cc7f264c656db1bdc27494e
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-03-12 05:05:30 +0700

    memperbaiki route dashboard

--- 20220140064-todo/routes/web.php
+++ 20220140064-todo/routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/20220140064-todo.test/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
